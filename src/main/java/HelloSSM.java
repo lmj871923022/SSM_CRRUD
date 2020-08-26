5c5987043f23c1aa49f974b6826a83bc8c515073
liumingchieh <mjliu_2333@stu.xidian.edu.cn>
2020-08-27 00:25:54 +0800

    For testing my IDEA options,Add a print line.

--- src/main/java/HelloSSM.java
+++ src/main/java/HelloSSM.java
@@ -2,5 +2,6 @@ public class HelloSSM {
     public static void main(String[] args) {
         System.out.println("fuck you once more!!!");
         System.out.println("Hello SSH keys!!");
+        System.out.println("Try to use git tools of IDEA!");
     }
 }
